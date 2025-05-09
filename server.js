364a2de2b8f8bf37cc272a441ab222b15c83a4a7
webdevelopersinkenya <mirr22006@byui.edu>
2025-05-09 12:43:48 +0300

    added other pages

--- server.js
+++ server.js
@@ -20,20 +20,33 @@ app.set("views", path.join(__dirname, "views"));
 
 // Static files
 app.use(staticRoutes);
+app.use(express.static('public'));
 
-// Routes
+// Main Routes
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
+});
+
+app.get("/custom", (req, res) => {
+  res.render("custom", { title: "Custom Vehicles" });
+});
+
+app.get("/sidan", (req, res) => {
+  res.render("sidan", { title: "Sedan Vehicles" });
+});
+
+app.get("/suv", (req, res) => {
+  res.render("suv", { title: "SUV Vehicles" });
+});
+
+app.get("/track", (req, res) => {
+  res.render("track", { title: "Truck Vehicles" });
 });
 
 // Server
 const port = process.env.PORT || 5500;
 const host = process.env.HOST || "localhost";
 
-
 app.listen(port, () => {
   console.log(`App listening at http://${host}:${port}`);
 });
-
-
-
