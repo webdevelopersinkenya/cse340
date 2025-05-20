fbf66dd0a4672cb498379aaa872383c80171a13c
webdevelopersinkenya <mirr22006@byui.edu>
2025-05-20 15:54:26 +0300

    learning activityw03

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
@@ -11,6 +12,9 @@ const path = require("path");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const staticRoutes = require("./routes/static");
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+
 
 // View engine and layout setup
 app.set("view engine", "ejs");
@@ -23,9 +27,7 @@ app.use(staticRoutes);
 app.use(express.static('public'));
 
 // Main Routes
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
-});
+app.get("/", baseController.buildHome);
 
 app.get("/custom", (req, res) => {
   res.render("custom", { title: "Custom Vehicles" });
@@ -42,6 +44,7 @@ app.get("/suv", (req, res) => {
 app.get("/track", (req, res) => {
   res.render("track", { title: "Truck Vehicles" });
 });
+app.use("/inv", inventoryRoute)
 
 // Server
 const port = process.env.PORT || 5500;
