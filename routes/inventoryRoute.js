984cc6621d942e64fa63b8c852ce9fb6274e4c95
webdevelopersinkenya <mirr22006@byui.edu>
2025-05-23 18:06:32 +0300

    changes

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,13 +4,15 @@ const express = require("express");
 const router = express.Router();
 const invController = require("../controllers/invController");
 const utilities = require('../utilities');
+const inventoryModel = require('../models/inventory-model');
 
 router.get("/detail/:inv_id", 
   utilities.handleErrors(invController.buildDetailView)
 );
 
-module.exports = router;
-
 router.get("/error/test", (req, res, next) => {
   throw new Error("Intentional error for testing 500");
+
+  module.exports = router;
 });
+
