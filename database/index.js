a8d4e23e28e99672bb8d4a9b34b6d5e7b4b4e2ac
webdevelopersinkenya <mirr22006@byui.edu>
2025-05-24 16:48:14 +0300

    changes

--- database/index.js
+++ database/index.js
@@ -3,7 +3,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || "postgresql://gab340:gab340@localhost:5500/gab340",
-  // If you have SSL config for production, add it here
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
