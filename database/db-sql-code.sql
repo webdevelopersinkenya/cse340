6540828039505e9d439ee8a96cf2a701ad61b216
webdevelopersinkenya <mirr22006@byui.edu>
2025-06-18 10:57:28 +0300

    changes

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -98,15 +98,18 @@ VALUES('custom'),
 --inventory Data
 
 SELECT * FROM public.inventory
-ORDER BY inv_id ASC 
+ORDER BY inv_id ASC;
+
 
 -- Modify GM Hummer to read huge interior rather than small interior.
 UPDATE inventory
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE inv_model = 'GM Hummer';
 
+
 -- Update inventory
 UPDATE inventory
 SET
   inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
   inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
